000127e458f4f47bbd4bccbe4883949910480ad5
Ali Nauman <ali.nauman@arbisoft.com>
2022-04-20 23:23:37 +0500

    feat: experiment with ngOnChanges and ngModelChange

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { CartItem } from '../models/cart-item';
 
 @Component({
@@ -6,16 +6,30 @@ import { CartItem } from '../models/cart-item';
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.scss']
 })
-export class CartItemComponent implements OnInit {
+export class CartItemComponent implements OnInit, OnChanges {
   @Input() item: CartItem;
 
   @Output() quantityUpdate = new EventEmitter();
   @Output() remove = new EventEmitter();
 
+  quantity: number;
+
   constructor() {
     this.item = new CartItem();
+    this.quantity = 0;
   }
 
   ngOnInit(): void {
+    this.quantity = this.item.quantity;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['item']) {
+      this.quantity = changes['item'].currentValue.quantity;
+    }
+  }
+
+  handleUpdate(event: Event) {
+    this.quantityUpdate.emit(event);
   }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -19,16 +19,21 @@ export class CartComponent implements OnInit {
   constructor(private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
-    this.cart = this.cartService.fetch()
+    this.cart = this.cartService.fetch();
+
+    if (!this.cart.length) {
+      alert('No items present in cart');
+      this.router.navigate(['/']);
+    }
   }
 
   getTotal(): string {
     return this.cart.map(item => item.product.price * item.quantity).reduce((a, b) => a + b).toFixed(2);
   }
 
-  handleQuantityUpdate(event: Event, product: Product) {
-    const target = event.target as HTMLInputElement;
-    this.cartService.update(product, Number(target.value));
+  handleQuantityUpdate(quantity: number, product: Product) {
+    this.cartService.update(product, quantity);
+    this.cart = this.cartService.fetch()
   }
 
   handleRemove(product: Product) {
